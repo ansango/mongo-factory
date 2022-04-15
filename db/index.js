7a8852ed679293334b689726e131c71dbe6882c0
ansango <anibalsantosgo@gmail.com>
2022-04-16 01:26:57 +0200

    up

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const { MongoClient } = require("mongodb");
 
-const URI = `mongodb+srv://user:pass@url.mongodb.net/collection?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://<username>:${encodeURIComponent(
+  "<password>"
+)}@<cluster>.mongodb.net/<database>?retryWrites=true&w=majority`;
 
 const client = new MongoClient(URI, {
   useNewUrlParser: true,
