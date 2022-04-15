0ee35925b1e7ed166bb01edd9c3ffe495207e144
Aníbal Santos Gómez <anibalsantosgo@gmail.com>
2022-04-16 01:25:24 +0200

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const URI = `mongodb+srv://anibalsantos:721989Ansan@cluster0.knytw.mongodb.net/robocooker?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://user:pass@url.mongodb.net/collection?retryWrites=true&w=majority`;
 
 const client = new MongoClient(URI, {
   useNewUrlParser: true,
